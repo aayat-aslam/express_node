4f1b0201da22eb0bf0e019ee9eef65c45082d2a4
aayat-aslam <wwww.aslam004@gmail.com>
2025-05-12 19:35:50 +0530

    refactor(subscription): enhance code readability with comments and formatting

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,20 +1,29 @@
-import mongoose, {Schema} from "mongoose";
+// Importing Mongoose and Schema constructor for defining the schema
+import mongoose, { Schema } from "mongoose";
+
+// Import the User model to reference it in the schema
 import { User } from "./user.model";
 
+// Define the schema for a subscription relationship between users
 const subscriptionSchema = new Schema(
     {
+        // 'subscriber' is the user who subscribes to a channel (another user)
         subscriber: {
-            type: Schema.Types.ObjectId,
-            ref: User
+            type: Schema.Types.ObjectId, // Stores MongoDB ObjectId
+            ref: User                   // Refers to the 'User' model
         },
+
+        // 'channel' is the user who is being subscribed to
         channel: {
-            type: Schema.Types.ObjectId,
-            ref: User
+            type: Schema.Types.ObjectId, // Stores MongoDB ObjectId
+            ref: User                   // Refers to the 'User' model
         }
     },
     {
+        // Automatically adds 'createdAt' and 'updatedAt' fields to track changes
         timestamps: true
     }
-)
+);
 
+// Create and export the Mongoose model named 'Subscription' using the schema
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
